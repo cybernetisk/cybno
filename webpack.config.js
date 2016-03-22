6e548080d3d1b8ba0adcbf89f7d4aecf1d630084
Henrik Steen <henrist@henrist.net>
2016-03-22 15:36:48 +0100

    Set publicpath for webpack so devserver works

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'public/build/'),
+    publicPath: 'build/',
     filename: 'bundle.js',
   },
   module: {
