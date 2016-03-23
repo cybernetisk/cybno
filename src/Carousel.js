b473993bd1375f18ed2acc9f324fcfbf181ff549
Henrik Steen <henrist@henrist.net>
2016-03-23 15:46:33 +0100

    Update carousel images

--- src/Carousel.js
+++ src/Carousel.js
@@ -7,21 +7,25 @@ export default class Carousel extends React.Component {
   render() {
     const settings = {
       autoplay: true,
-      autoplaySpeed: 5000,
+      autoplaySpeed: 6000,
       arrows: false,
       dots: true,
       infinite: true,
+      lazyLoad: true,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1
     };
 
     const images = [
-      ['img/kafe.jpg', 'Kafé i Escape hver ukedag kl 10 til kl 15.15! Kom innom for en billig studentkaffe!'],
-      ['img/fest.jpg', 'Fest'],
-      ['img/kilofest.jpg', 'Kilofest'],
-      ['img/fredagspub.jpg', 'Fredagspub'],
-      ['img/av.jpg', 'AV-utstyr']
+      // image ratio: 1.85:1, preferred with: 1170px (height: 632px)
+      //['KAFEBILDE', 'Kafé i Escape hver ukedag kl 10 til kl 15.15! Kom innom for en billig studentkaffe!'],
+      ['http://cyb.no/2016-assets/vannspeil.jpg', 'Studentkafeen og -baren Escape ligger i kjelleren på Ole-Johan Dahls hus ved Forskningsparken.'],
+      ['http://cyb.no/2016-assets/pub.jpg', 'Escape tilbyr en romslig og hyggelig samlingsplass for studenter.'],
+      ['http://cyb.no/2016-assets/kilofest.jpg', 'Kilofest'],
+      ['http://cyb.no/2016-assets/faglig-escape.jpg', 'Vi arrangerer både sosiale og faglige eventer.'],
+      ['http://cyb.no/2016-assets/styrekurs.jpg', 'Som frivillig får man oppleve ting også utenfor instituttet, som her fra teambuilding på styrekurs for hoved- og kjellerstyret på farmasihytta.'],
+      ['http://cyb.no/2016-assets/projektor.jpg', 'AV-utstyr']
     ]
 
     return (
