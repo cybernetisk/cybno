c51c50c381ba5d7711cf867dd289016273e03168
Thor K. H <thor@roht.no>
2018-02-20 20:57:17 +0100

    Upgraded webpack and dependencies
    Among other pains...

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,50 @@
-var webpack = require('webpack');
-var path = require('path');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-var production = process.env.NODE_ENV === 'production';
-var noop = function () {};
+const production = process.env.NODE_ENV === 'production';
+const noop = function () {};
+const extractSass = new ExtractTextPlugin({
+  filename: "[name].css",
+  disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js',
+  ],
   output: {
     path: path.resolve(__dirname, 'public/build/'),
     publicPath: 'build/',
     filename: 'bundle.js',
   },
+  devServer: {
+    contentBase: './public',
+    hot: true,
+  },
   module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot', 'babel-loader'] },
-      { test: /\.json$/, loader: 'json' },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
-      { test: /\.scss$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader') },
-      { test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot-loader/webpack', 'babel-loader'] },
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.scss$/,
+        use: extractSass.extract({
+          use: [{
+            loader: 'css-loader'
+          },{
+            loader: 'sass-loader',
+          }],
+          fallback: 'style-loader'
+        })},
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          mimetype: "application/font-woff"
+        }
+      },
       { test: /\.(ttf|eot|svg|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
     ]
   },
@@ -26,14 +52,16 @@ module.exports = {
     new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /nb/),
     new ExtractTextPlugin("[name].css"),
 
-    new webpack.ProvidePlugin({
-      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-    }),
+    // imports and exports loader
+    //new webpack.ProvidePlugin({
+    //  'fetch': 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
+    //}),
 
-    production && new webpack.optimize.DedupePlugin() || noop,
     production && new webpack.optimize.UglifyJsPlugin({sourceMap: false}) || noop,
-    production && new webpack.NoErrorsPlugin() || noop,
-    production && new webpack.optimize.OccurenceOrderPlugin() || noop,
+    production && new webpack.NoEmitOnErrorsPlugin() || noop,
+
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
 
     // removes a lot of debugging code in React
     production && new webpack.DefinePlugin({
